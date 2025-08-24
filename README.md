e85fc710a7b459d675a5aad7fbdf1b750b46f2ca
Aldo431 <aldoluna75@gmail.com>
2025-08-24 18:02:24 -0400

    feat: se agrega logica para confirmar el formulario

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
-# desarrollo_web_Aldo_Luna
-Repositorio para las tareas del curso Desarrollo de Aplicaciones Web
+# Tarea 1 - Desarrollo Web
+   
+## Descripción
+Esta tarea incluye la implementación de una página HTML con CSS.
+   
+## Decisiones tomadas
+- Utilicé flexbox para organizar los elementos.
+- Agregué comentarios en el código para mayor claridad.
+```
--- formulario.js
+++ formulario.js
@@ -1,26 +1,57 @@
 
 const regiones = {
-    "Arica-y-parinacota": ["Arica", "Camarones", "Putre", "General Lagos"],
-    "Tarapaca": ["Iquique", "Alto Hospicio", "Pozo Almonte", "Pica"],
-    "Antofagasta": ["Antofagasta", "Mejillones", "Taltal", "Calama", "San Pedro de Atacama"],
-    "Atacama": ["Copiapó", "Caldera", "Tierra Amarilla", "Vallenar", "Huasco"],
-    "Coquimbo": ["La Serena", "Coquimbo", "Ovalle", "Illapel", "Salamanca"],
-    "Valparaiso": ["Valparaíso", "Viña del Mar", "Quilpué", "Villa Alemana", "San Antonio"],
-    "Metropolitana": ["Santiago", "Providencia", "Ñuñoa", "Las Condes", "Maipú", "La Florida",
-    "Puente Alto", "La Reina", "Vitacura", "Lo Barnechea", "Pedro Aguirre Cerda",
-    "San Miguel", "Pudahuel", "Peñalolén", "La Granja", "Macul", "Ñuñoa",
-    "Recoleta", "Quinta Normal", "San Joaquín", "San Ramón", "Cerrillos",
-    "El Bosque", "Huechuraba", "Independencia", "La Cisterna", "Lo Espejo",
-    "Lo Prado", "Pedro Aguirre Cerda", "Penaflor", "San Bernardo", "Talagante"],
-    "Ohiggins": ["Rancagua", "San Fernando", "Rengo", "Machalí", "Pichilemu"],
-    "Maule": ["Talca", "Curicó", "Linares", "Maule", "San Javier"],
-    "Ñuble": ["Chillán", "Chillán Viejo", "Quirihue", "San Carlos", "Bulnes"],
-    "Biobio": ["Concepción", "Talcahuano", "Hualpén", "Chillán", "Los Ángeles", "Coronel"],
-    "Araucania": ["Temuco", "Villarrica", "Angol", "Pucón", "Lautaro"],
-    "Los-rios": ["Valdivia", "La Unión", "Río Bueno", "Lanco", "Paillaco"],
-    "Los-lagos": ["Puerto Montt", "Puerto Varas", "Castro", "Osorno", "Ancud"],
-    "Aysen": ["Coyhaique", "Puerto Aysén", "Chile Chico", "Cisnes", "Guaitecas"],
-    "Magallanes": ["Punta Arenas", "Puerto Natales", "Porvenir", "Puerto Williams", "Cabo de Hornos"]
+    "Arica y parinacota": ["Arica", "Camarones", "Putre", "General Lagos"],
+    "Tarapaca": ["Iquique", "Alto Hospicio", "Pozo Almonte", "Pica", "Huara", "Camiña", "Colchane"],
+    "Antofagasta": ["Antofagasta", "Mejillones", "Taltal", "Calama", "San Pedro de Atacama", "Tocopilla", "María Elena", "Ollagüe", "Sierra Gorda"],
+    "Atacama": ["Copiapó", "Caldera", "Tierra Amarilla", "Vallenar", "Huasco", "Chañaral", "Diego de Almagro", "Freirina", "Alto del Carmen"],
+    "Coquimbo": ["La Serena", "Coquimbo", "Ovalle", "Illapel", "Salamanca", "Vicuña", "Andacollo", "Los Vilos", "Combarbalá", "Punitaqui", "Paihuano", "Río Hurtado", "Monte Patria", "La Higuera", "Mincha"],
+    "Valparaíso": [
+        "Valparaíso", "Viña del Mar", "Quilpué", "Villa Alemana", "San Antonio", "Quillota", "La Calera", "Limache", "Olmué", "Puchuncaví", "Quintero", "Casablanca", "Concón", "Cartagena",
+        "Algarrobo", "El Quisco", "El Tabo", "Juan Fernández", "Petorca", "Cabildo", "Papudo", "La Ligua", "Zapallar", "Putaendo", "Santa María", "San Felipe", "Catemu", "Llay Llay",
+        "Nogales", "Hijuelas", "Isla de Pascua", "Santo Domingo", "Calle Larga", "Rinconada", "San Esteban"
+    ],
+    "Ohiggins": [
+        "Rancagua", "San Fernando", "Rengo", "Machalí", "Pichilemu", "San Vicente", "Santa Cruz", "Requínoa", "Mostazal", "Navidad", "Codegua", "Graneros", "Olivar", "Doñihue", "Coinco",
+        "Coltauco", "Quinta de Tilcoco", "Las Cabras", "Peumo", "Pichidegua", "Malloa", "La Estrella", "Marchigüe", "Litueche", "Paredones", "Peralillo", "Placilla", "Chimbarongo",
+        "Palmilla", "Nancagua", "Pumanque", "Chépica", "Lolol"
+    ],
+    "Maule": [
+        "Talca", "Curicó", "Linares", "Maule", "San Javier", "Constitución", "Parral", "Cauquenes", "Molina", "Teno", "Romeral", "Rauco", "Sagrada Familia", "Hualañé", "Vichuquén", "Licantén",
+        "Río Claro", "Curepto", "Pelarco", "Pencahue", "San Clemente", "Empedrado", "San Rafael", "Colbún", "Villa Alegre", "Yerbas Buenas", "Longaví", "Retiro", "Chanco", "Pelluhue"
+    ],
+    "Ñuble": [
+        "Chillán", "Chillán Viejo", "San Carlos", "Quirihue", "Bulnes", "Coihueco", "Pemuco", "Ñiquén", "Cobquecura", "San Fabián", "Ninhue", "Trehuaco", "San Nicolás", "Portezuelo", "Pinto",
+        "Coelemu", "San Ignacio", "Ránquil", "Quillón", "El Carmen", "Yungay"
+    ],
+    "Biobío": [
+        "Concepción", "Talcahuano", "Hualpén", "Chiguayante", "Los Ángeles", "Coronel", "Lota", "San Pedro de la Paz", "Tomé", "Florida", "Penco", "Hualqui", "Santa Juana", "Cabrero", "Yumbel",
+        "Tucapel", "Antuco", "San Rosendo", "Laja", "Quilleco", "Nacimiento", "Negrete", "Santa Bárbara", "Quilaco", "Mulchén", "Alto Biobío", "Arauco", "Curanilahue", "Los Álamos", "Lebu",
+        "Cañete", "Contulmo", "Tirúa"
+    ],
+    "Araucanía": [
+        "Temuco", "Padre Las Casas", "Villarrica", "Pucón", "Angol", "Nueva Imperial", "Carahue", "Traiguén", "Lautaro", "Victoria", "Cholchol", "Curacautín", "Lonquimay", "Melipeuco", "Cunco",
+        "Perquenco", "Galvarino", "Freire", "Pitrufquén", "Teodoro Schmidt", "Gorbea", "Tolten", "Curarrehue", "Loncoche", "Puerto Saavedra", "Ercilla", "Renaico", "Collipulli", "Los Sauces",
+        "Purén", "Lumaco"
+    ],
+    "Los Ríos": [
+        "Valdivia", "La Unión", "Río Bueno", "Lanco", "Mariquina", "Panguipulli", "Corral", "Paillaco", "Futrono", "Lago Ranco", "Máfil", "Los Lagos"
+    ],
+    "Los Lagos": [
+        "Puerto Montt", "Puerto Varas", "Castro", "Osorno", "Ancud", "Quellón", "Frutillar", "Fresia", "Chaitén", "Puyehue", "Río Negro", "Purranque", "Puerto Octay", "Maullín", "Calbuco",
+        "Cochamó", "Quemchi", "Dalcahue", "Curaco de Vélez", "Chonchi", "Queilén", "Quinchao", "Puqueldón", "Futaleufú", "Palena", "Hualaihué", "San Pablo", "San Juan"
+    ],
+    "Aysén": [
+        "Coyhaique", "Puerto Aysén", "Chile Chico", "Cisnes", "Guaitecas", "Lago Verde", "Río Ibáñez", "Cochrane", "Tortel", "O’Higgins"
+    ],
+    "Magallanes": [
+        "Punta Arenas", "Puerto Natales", "Porvenir", "Puerto Williams", "Torres del Paine", "Laguna Blanca", "San Gregorio", "Río Verde", "Primavera", "Timaukel", "Antártica"
+    ],
+    "Metropolitana": [
+        "Santiago", "Providencia", "Ñuñoa", "Las Condes", "Maipú", "La Florida", "Puente Alto", "La Reina", "Vitacura", "Lo Barnechea", "Pedro Aguirre Cerda", "San Miguel", "Pudahuel",
+        "Peñalolén", "La Granja", "Macul", "Recoleta", "Quinta Normal", "San Joaquín", "San Ramón", "Cerrillos", "El Bosque", "Huechuraba", "Independencia", "La Cisterna", "Lo Espejo", "Lo Prado",
+        "Cerro Navia", "Estación Central", "Peñaflor", "San Bernardo", "Talagante", "Buin", "Calera de Tango", "Padre Hurtado", "Tiltil", "Colina", "Lampa", "Conchalí", "Quilicura", "Renca",
+        "San José de Maipo", "Pirque", "Isla de Maipo", "Curacaví", "María Pinto", "Melipilla", "San Pedro", "Alhué", "El Monte"
+    ]
 };
 
 // Inputs de region y comuna
@@ -43,7 +74,7 @@ regionInput.addEventListener("change", () => {
     const comunas = regiones[regionInput.value] || [];
     comunas.forEach(comuna => {
         const option = document.createElement("option");
-        option.value = comuna.toLowerCase().replace(/\s+/g, '-');
+        option.value = comuna;
         option.textContent = comuna;
         ComunaInput.appendChild(option);
     });
@@ -101,7 +132,7 @@ fotosContainer.addEventListener("click", (e) => {
         }
 
         const nuevoItem = items[0].cloneNode(true);
-        nuevoItem.querySelector(".foto-input").value = ""; // limpiar archivo
+        nuevoItem.querySelector(".foto-input").value = ""; 
         fotosContainer.appendChild(nuevoItem);
     }
 });
@@ -199,8 +230,6 @@ const validarForm = () => {
     } else {
         telInput.style.borderColor = "";
     }
-    
-    // validación de los id o url informados
     id_url_Input.forEach(input => {
         if (input.offsetParent !== null) {
             const id_url = input.value.trim();
@@ -240,6 +269,7 @@ const validarForm = () => {
     } else {
         document.getElementById("tipo-unidad-input").style.border = "";
     }
+    
     if (!validarFechaDisponible()) {
         msg += "n";
         fechaInput.style.borderColor = "red";        
@@ -253,9 +283,16 @@ const validarForm = () => {
     } else {
         fotosContainer.style.border = "";
     }
-    if (msg === ""){
 
-     }
+    if (msg === ""){
+       isValid = true;
+       window.open(
+        "confirmacion.html",   
+        "Confirmacion",  
+        "width=500,height=300,top=150,left=400,resizable=no"
+        );
+    }
+
     return isValid;
 }
 
